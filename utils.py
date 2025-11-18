583b4004d36ca6a0999f6c0d3bef460ef5e5ddf5
lmw <lmw19974233932@gmail.com>
2025-11-18 19:07:18 +0800

    定义模型初始化工具

--- utils.py
+++ utils.py
@@ -6,3 +6,21 @@ import os
 from py2neo import Graph
 from dotenv import load_dotenv
 load_dotenv()
+
+def get_embeddings_model():
+    model_map = {
+        'openai': OpenAIEmbeddings(
+            model=os.getenv('OPENAI_EMBEDDINGS_MODEL')
+        )
+    }
+    return model_map.get(os.getenv('EMBEDDINGS_MODEL'))
+
+def get_llm_model():
+    model_map = {
+        'openai': ChatOpenAI(
+            model=os.getenv('OPENAI_LLM_MODEL'),
+            temperature=os.getenv('TEMPERATURE'),
+            max_tokens=os.getenv('MAX_TOKENS'),
+        )
+    }
+    return model_map.get(os.getenv('LLM_MODEL'))
